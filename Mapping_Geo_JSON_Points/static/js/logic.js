1473fce67e6a0532810bcc08b352e80a65cf46f8
Bev Edwards-Smith <bedwardssmith@yahoo.ca>
2021-03-21 15:24:36 -0400

    updated files

--- Mapping_Geo_JSON_Points/static/js/logic.js
+++ Mapping_Geo_JSON_Points/static/js/logic.js
@@ -1,33 +1,45 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-        40.7, -94.5
-    ],
-    zoom: 4
-});
+let map = L.map("mapid").setView([37.5, -122.5], 10);
 
-// Get data from cities.js
-let cityData = cities;
+// Add GeoJSON data.
+let sanFranAirport =
+{
+    "type":"FeatureCollection","features":[{
+        "type": "Feature",
+        "properties":{
+            "id":"3469",
+            "name":"San Francisco International Airport",
+            "city":"San Francisco",
+            "country":"United States",
+            "faa":"SFO",
+            "icao":"KSFO",
+            "alt":"13",
+            "tz-offset":"-8",
+            "dst":"A",
+            "tz":"America/Los_Angeles"
+        },
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375, 37.61899948120117]
+        }
+    }
+    ]
+};
 
 
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function (city) {
-    console.log(city)
-});
+// Grabbing our GeoJSON data
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function (feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h2>Airport Name:" + feature.properties.name + "</h2><h3>Airport Code:" + feature.properties.faa + "</h2>");
+    }
+}).addTo(map);
 
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function (city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-        .addTo(map);
-});
-
+// We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
