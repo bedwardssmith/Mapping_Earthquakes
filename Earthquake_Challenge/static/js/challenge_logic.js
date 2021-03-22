2bb56ba9f718a8f9cb5a1861679e23750d7c3a59
Bev Edwards-Smith <bedwardssmith@yahoo.ca>
2021-03-22 18:03:05 -0400

    updated for tectonic plate

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -30,11 +30,13 @@ let baseMaps = {
 
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
+let tectonicPlate = new L.LayerGroup();
 
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes
+    "Earthquakes": allEarthquakes,
+    "Techtonic": tectonicPlate
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -139,9 +141,17 @@ legend.onAdd = function() {
   // Finally, we our legend to the map.
   legend.addTo(map);
 
+  // Create a style for the lines of tectonic plate
+    let tecstyle = {
+        color: "red",
+        weight: 2
+    };
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(() {
-    
-  });
+    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (data) {
+        console.log(data);
+        // Create a GeoJSON layer with the retreived data
+        L.geoJson(data, tecstyle).addTo(tectonicPlate);
+    });
 });
+tectonicPlate.addTo(map);
