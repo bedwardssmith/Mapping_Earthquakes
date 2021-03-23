ece80678d6d445dd03d7cb5ba1004f37fa22fae2
Bev Edwards-Smith <bedwardssmith@yahoo.ca>
2021-03-23 07:17:17 -0400

    update images

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -38,7 +38,7 @@ let majorEarthquakes30Days = new L.LayerGroup();
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
     "Earthquakes": allEarthquakes,
-    "Tectonic": tectonicPlate,
+    "Tectonic Plate": tectonicPlate,
     "Major Earthquakes": majorEarthquakes,
     "Major Earthquakes Past 30 Days": majorEarthquakes30Days
 };
@@ -232,7 +232,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geo
         };
     }
 
-    // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+    // Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
     function getColor(magnitude) {
         if (magnitude > 5) {
             return "#ea2c2c";
@@ -243,7 +243,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geo
         return "#ee9c00";
     }
 
-    // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+    // Use the function that determines the radius of the earthquake marker based on its magnitude.
     function getRadius(magnitude) {
         if (magnitude === 0) {
             return 1;
@@ -251,7 +251,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geo
         return magnitude * 4;
     }
 
-    // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+    // Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
     // sets the style of the circle, and displays the magnitude and location of the earthquake
     //  after the marker has been created and styled.
     L.geoJson(data, {
